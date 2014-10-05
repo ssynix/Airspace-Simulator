43918c6b012fc83405a296aa16d6ddf15a4c643f
Shayan Sayahi <ss.synix@gmail.com>
2014-10-05 14:34:19 -0500

    Autocommit at Sun Oct  5 14:34:19 CDT 2014

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # @Author: Synix
 # @Date:   2014-09-25 09:16:40
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-05 14:34:06
+# @Last Modified time: 2014-10-05 14:34:18
 
 #/usr/bin/env python
 """
@@ -74,7 +74,8 @@ class PlaneSprite(pygame.sprite.Sprite):
         center = self.rect.center
 
         # Scale down the icon
-        scale = 50. / self.rect.width
+        iconsize = 50.
+        scale = iconsize / self.rect.width
 
         # Calculate heading and rotate the icon accordingly
         if self.plane.speed.x is 0.:
