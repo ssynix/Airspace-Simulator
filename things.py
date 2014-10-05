5e2bffb2df7ace1b62de06fd2aedac7d25b129ca
Shayan Sayahi <ss.synix@gmail.com>
2014-10-05 16:17:09 -0500

    Autocommit at Sun Oct  5 16:17:09 CDT 2014

--- things.py
+++ things.py
@@ -3,7 +3,7 @@
 # @Author: Synix
 # @Date:   014-10-01 05:52:51
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-05 16:12:40
+# @Last Modified time: 2014-10-05 16:17:04
 
 
 class Vector:
@@ -52,6 +52,9 @@ class Plane:
 
     def squareDistance(self, other):
         return sum((other.position - self.position) ** 2)
+
+    def int2D(self):
+        return (int(self.position.x), int(self.position.y))
 
 
 if __name__ == '__main__':
