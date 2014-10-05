f20daf35428c0b6e0106a88c46085c9927a71410
Shayan Sayahi <ss.synix@gmail.com>
2014-10-04 20:21:20 -0500

    Autocommit at Sat Oct  4 15:04:31 CDT 2014

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # @Author: Synix
 # @Date:   2014-09-25 09:16:40
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-04 20:20:36
+# @Last Modified time: 2014-10-04 20:21:15
 
 #/usr/bin/env python
 """
@@ -69,11 +69,11 @@ class PlaneSprite(pygame.sprite.Sprite):
         # print self.plane.position, self.plane.destination, self.plane.speed, heading
         # self.image = pygame.transform.rotate(self.image, heading)
         # self.image.get_rect().center = center
-		x_cntr, y_cntr = self.image.get_rect().center
+        x_cntr, y_cntr = self.image.get_rect().center
 
-		pos_org = (x_cntr - self.image.get_rect().width / 2, y_cntr - self.image.get_rect().height / 2)
-		image_rotated = pygame.transform.rotate(self.image, heading) #rotate image
-		pos_new = (x_pos_org - image_rotated.get_rect().width / 2, y_pos_org - image_rotated.get_rect().height / 2)
+        pos_org = (x_cntr - self.image.get_rect().width / 2, y_cntr - self.image.get_rect().height / 2)
+        image_rotated = pygame.transform.rotate(self.image, heading) #rotate image
+        pos_new = (x_pos_org - image_rotated.get_rect().width / 2, y_pos_org - image_rotated.get_rect().height / 2)
 
     def update(self):
         "move the fist based on the mouse position"
