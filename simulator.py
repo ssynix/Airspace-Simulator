68a413213199c821bee4ce3ddc77d763a3202fa4
Shayan Sayahi <ss.synix@gmail.com>
2014-10-05 14:16:43 -0500

    Autocommit at Sun Oct  5 14:16:43 CDT 2014

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # @Author: Synix
 # @Date:   2014-09-25 09:16:40
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-05 14:15:11
+# @Last Modified time: 2014-10-05 14:16:41
 
 #/usr/bin/env python
 """
@@ -84,7 +84,7 @@ class PlaneSprite(pygame.sprite.Sprite):
         # print self.rect.size, self.plane.position
         # print self.rect.topleft
         inv = pygame.Surface((800,600))
-        inv.fill((50,100,50,100))
+        inv.fill((50,100,50))
         self.image.blit(inv, (0,0), None, BLEND_ADD)
         # heightToColor = int((self.plane.position.z / 6e3) * 155 + 90)
         # self.colored = self.image.copy()
