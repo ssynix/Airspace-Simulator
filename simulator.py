8d6492d3976e71ceb71b31fb40080fb49fd843f6
Shayan Sayahi <ss.synix@gmail.com>
2014-10-05 14:25:41 -0500

    Autocommit at Sun Oct  5 14:25:41 CDT 2014

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # @Author: Synix
 # @Date:   2014-09-25 09:16:40
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-05 14:24:53
+# @Last Modified time: 2014-10-05 14:25:37
 
 #/usr/bin/env python
 """
@@ -85,11 +85,13 @@ class PlaneSprite(pygame.sprite.Sprite):
             heading += 180
         self.image = pygame.transform.rotate(self.image, heading)
 
+        # Change the plane's color according to its height
         heightToColor = int((self.plane.position.z / 6e3) * 155 + 90)
         heightColor = pygame.Surface(self.rect.size)
         heightColor.fill((heightToColor, heightToColor, heightToColor))
         self.image.blit(heightColor, (0,0), None, BLEND_MIN)
 
+        # Restore the image to its original coordinates
         self.rect = self.image.get_rect()
         self.rect.center = center
 
