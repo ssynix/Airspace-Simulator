be56534a0e5b467094e90bd175237ea5e8fe39c6
Shayan Sayahi <ss.synix@gmail.com>
2014-10-05 16:49:45 -0500

    Autocommit at Sun Oct  5 16:49:45 CDT 2014

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # @Author: Synix
 # @Date:   2014-09-25 09:16:40
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-05 16:49:38
+# @Last Modified time: 2014-10-05 16:49:45
 
 #/usr/bin/env python
 """
@@ -118,7 +118,7 @@ def main():
 #Create The Backgound
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill((200, 200, 150))
+    background.fill((240, 200, 150))
 
 # #Put Text On The Background, Centered
 #     if pygame.font:
