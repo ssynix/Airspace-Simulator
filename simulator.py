a4c9f5cdb8da62f8cd9d9399a2b876cac302598f
Shayan Sayahi <ss.synix@gmail.com>
2014-10-04 20:54:46 -0500

    Autocommit at Sat Oct  4 15:04:31 CDT 2014

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # @Author: Synix
 # @Date:   2014-09-25 09:16:40
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-04 20:54:37
+# @Last Modified time: 2014-10-04 20:54:46
 
 #/usr/bin/env python
 """
@@ -62,7 +62,7 @@ class PlaneSprite(pygame.sprite.Sprite):
         self.image, self.rect = load_image('plane.png', -1)
         center = self.rect.center
         self.image = pygame.transform.scale(self.image, (50, 50))
-        self.image.get_rect().center = (1,1)
+        self.image.get_rect().center = center
         self.plane = Plane(randint(0, 800), randint(0, 600), randint(1e3, 6e3))
         self.plane.setCourse(randint(0, 800), randint(0, 600), randint(1e3, 6e3), 450.)
 
