5ec924b6f20d04a2a4ea17fa9f195f3df2f40dbd
Shayan Sayahi <ss.synix@gmail.com>
2014-10-04 20:20:36 -0500

    Autocommit at Sat Oct  4 15:04:31 CDT 2014

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # @Author: Synix
 # @Date:   2014-09-25 09:16:40
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-04 20:17:57
+# @Last Modified time: 2014-10-04 20:20:32
 
 #/usr/bin/env python
 """
@@ -64,11 +64,16 @@ class PlaneSprite(pygame.sprite.Sprite):
         self.plane = Plane(randint(0, 800), randint(0, 600), randint(1e3, 6e3))
         self.plane.setCourse(randint(0, 800), randint(0, 600), randint(1e3, 6e3), 450.)
 
-        center = self.image.get_rect().center
+        # center = self.image.get_rect().center
         heading = math.degrees(math.atan(1. * self.plane.speed.y/self.plane.speed.x))
-        print self.plane.position, self.plane.destination, self.plane.speed, heading
-        self.image = pygame.transform.rotate(self.image, heading)
-        self.image.get_rect().center = center
+        # print self.plane.position, self.plane.destination, self.plane.speed, heading
+        # self.image = pygame.transform.rotate(self.image, heading)
+        # self.image.get_rect().center = center
+		x_cntr, y_cntr = self.image.get_rect().center
+
+		pos_org = (x_cntr - self.image.get_rect().width / 2, y_cntr - self.image.get_rect().height / 2) 
+		image_rotated = pygame.transform.rotate(self.image, heading) #rotate image
+		pos_new = (x_pos_org - image_rotated.get_rect().width / 2, y_pos_org - image_rotated.get_rect().height / 2)
 
     def update(self):
         "move the fist based on the mouse position"
