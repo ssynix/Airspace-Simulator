61f38cc0c0121b4e05019c2a2635e6e68dffb3e9
Shayan Sayahi <ss.synix@gmail.com>
2014-10-05 16:23:36 -0500

    Autocommit at Sun Oct  5 16:23:36 CDT 2014

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # @Author: Synix
 # @Date:   2014-09-25 09:16:40
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-05 16:23:18
+# @Last Modified time: 2014-10-05 16:23:35
 
 #/usr/bin/env python
 """
@@ -147,7 +147,7 @@ def main():
         pygame.display.flip()
 
         def flash(sprite):
-            pygame.draw.circle(screen, (255, 0, 0), sprite.plane.int2Dpos(), 50, 3)
+            pygame.draw.circle(screen, (255, 0, 0), sprite.plane.int2Dpos(), 50, 1)
 
         collisions = ((flash(p1), flash(p2)) for (p1, p2) in product(allsprites, repeat=2) if p1.plane.squareDistance(p2.plane) < 50)
         pygame.time.delay(250)
