90f40b6faf04839275a71166de8d1c1d4cef71ed
Shayan Sayahi <ss.synix@gmail.com>
2014-10-04 22:41:14 -0500

    Autocommit at Sat Oct  4 15:04:31 CDT 2014

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # @Author: Synix
 # @Date:   2014-09-25 09:16:40
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-04 22:36:54
+# @Last Modified time: 2014-10-04 22:41:12
 
 #/usr/bin/env python
 """
@@ -84,7 +84,7 @@ class PlaneSprite(pygame.sprite.Sprite):
 
         heightToColor = (self.plane.position.z / 6e3) * 155 + 90
         print heightToColor
-        # color_surface(self.image, (heightToColor, heightToColor, heightToColor))
+        color_surface(self.image, (heightToColor, heightToColor, heightToColor))
 
         self.rect = self.image.get_rect()
         self.rect.center = center
