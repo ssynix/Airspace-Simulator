f09721b536a30f013e08ed1c678b03ac0c804e16
Shayan Sayahi <ss.synix@gmail.com>
2014-10-04 21:17:03 -0500

    Autocommit at Sat Oct  4 15:04:31 CDT 2014

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # @Author: Synix
 # @Date:   2014-09-25 09:16:40
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-04 21:16:55
+# @Last Modified time: 2014-10-04 21:17:03
 
 #/usr/bin/env python
 """
@@ -66,7 +66,7 @@ class PlaneSprite(pygame.sprite.Sprite):
         self.rect.center = (self.plane.position.x, self.plane.position.y)
         center = self.rect.center
 
-        # self.image = pygame.transform.smoothscale(self.image, (50, 50))
+        self.image = pygame.transform.smoothscale(self.image, (50, 50))
         heading = math.degrees(math.atan(1. * self.plane.speed.y/self.plane.speed.x))
         self.image = pygame.transform.rotate(self.image, heading)
 
