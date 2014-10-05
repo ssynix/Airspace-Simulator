5b7bf175140b4ecd61d3a26d3ac9024906efed59
Shayan Sayahi <ss.synix@gmail.com>
2014-10-05 16:27:04 -0500

    Autocommit at Sun Oct  5 16:27:04 CDT 2014

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # @Author: Synix
 # @Date:   2014-09-25 09:16:40
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-05 16:25:47
+# @Last Modified time: 2014-10-05 16:27:02
 
 #/usr/bin/env python
 """
@@ -122,7 +122,7 @@ def main():
 
 #Prepare Game Objects
     clock = pygame.time.Clock()
-    planes = [PlaneSprite() for i in range(12)]
+    planes = [PlaneSprite() for i in range(1)]
     allsprites = pygame.sprite.RenderPlain(planes)
 
 #Main Loop
