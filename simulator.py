816d4f7b42680b63f5df0a69482f059b1a8b130b
Shayan Sayahi <ss.synix@gmail.com>
2014-10-04 20:32:01 -0500

    Autocommit at Sat Oct  4 15:04:31 CDT 2014

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # @Author: Synix
 # @Date:   2014-09-25 09:16:40
 # @Last Modified by:   Synix
-# @Last Modified time: 2014-10-04 20:31:30
+# @Last Modified time: 2014-10-04 20:31:57
 
 #/usr/bin/env python
 """
@@ -105,7 +105,7 @@ def main():
     clock = pygame.time.Clock()
     planes = (PlaneSprite() for i in range(1))
     allsprites = pygame.sprite.RenderPlain(planes)
-    print (planes[0].plane.position.x, planes[0].plane.position.y), (planes[0].plane.destination.x, planes[0].plane.destination.y)
+    print planes[0].plane.position.x
     # pygame.draw.line(screen, (0,0,0), (planes[0].plane.position.x, planes[0].plane.position.y), (planes[0].plane.destination.x, planes[0].plane.destination.y), 5)
 
 #Main Loop
